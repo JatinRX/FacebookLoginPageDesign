8c006a9b3425906b47e606dc327ccfc5ffa060a2
JatinRX <44193179+JatinRX@users.noreply.github.com>
2019-04-26 11:57:06 +0530

    first design upload

---
+++ index.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html>
+	<head>
+		<link rel="shortcut icon" href="facebooklogo.jpg">
+		<link rel="stylesheet" type="text/css" href="facebookstyle.css">
+		<title>Facebook - log in or sign up</title>
+	
+	</head>
+	
+	<body>
+		<table style="width:101.3%; height:auto;padding:0px;margin:-9px;">
+			<tr style="height:5.5em;background-color:#3b5998;color:#FFFFFF;"> 
+				<td>
+					<table class="inner1" style="margin-upper:0px;margin-bottom:0px;">
+						<tr>
+							<td style="font-family:Verdana, Helvetica, sans-serif;font-size:2.4em;letter-spacing:-2px;vertical-align:bottom;padding:.5em 0em .4em .6em;"><b>facebook</b></td>
+							<td>
+								<table align="right" style="margin-right:1em;font-size:14px;">
+									<tr>
+									<form>
+										<td style="vertical-align:top;"><label style="display:block;">Email or Phone</label>
+											<input type="text" 	name="id" value=""><br>
+										</td>
+										<td>Password<br>
+											<input type="password" name="password" value=""><br>
+											<a id="forgotten" href=forgot-account>Forgotten account?</a>
+										</td>
+										<td><button id="login">Log In</button>
+										</td>
+									</form>
+									</tr>
+								</table>
+							</td>
+						</tr>
+					</table>
+				</td>
+			</tr>
+			<tr style="height:40em; background-color:#efeff4;color:#1d2129;"> 
+				<td>
+					<table class="inner1" style="margin-bottom:30px;" >
+						<tr>
+							<td	style="vertical-align:top;width:100%;">	
+								<table style="font-size:20px;width:100%;margin-left:10px;">
+									<tr><th>Recent logins</th></tr>
+									<tr><td>Click your picture or add an account.</td></tr>
+									<tr><td><img class="image" src="jatinfb.jpg" title="Jatin Garg" style="display:inline-block;">	<img class="image" src="newaccountfb.jpg" style="display:inline-block;"></td></tr>
+								</table>
+							</td>
+							<td>
+								<table id="newentry" align="right" style="font-size:20px; width:24em;">
+								<form>
+									<tr><th>Create a new account</th></tr>
+									<tr><td style="padding-bottom:10px;">It's free and always will be.</td></tr>
+									<tr><td><input class="style1" type="text" placeholder="First name" name="firstname" value="" style="display:inline-block;">&nbsp;
+											<input class="style1" type="text" placeholder="Surname" name="surname" value="" style="display:inline-block;"></td></tr>
+									<tr><td><input class="style1" type="text" placeholder="Mobile number or email address" name="newid" value="" style="display:inline-block;width:96.5%"></td></tr>
+									<tr><td><input class="style1" type="password" placeholder="New password" name="newpwd" style="display:inline-block;width:96.5%;">		</td></tr>
+									<tr><td style="padding-bottom:0px;">Birthday	</td></tr>
+									<tr><td><select class="style2" name="day" style="display:inline-block;width:4.7em; ">
+									<option value="0">Day</option>
+									<option value="1">1</option>
+									<option value="2">2</option>
+									<option value="3">3</option>
+									<option value="4">4</option>
+									<option value="5">5</option>
+									<option value="6">6</option>
+									<option value="7">7</option>
+									<option value="8">8</option>
+									<option value="9">9</option>
+									<option value="10">10</option>
+									<option value="11">11</option>
+									<option value="12">12</option>
+									<option value="13">13</option>
+									<option value="14">14</option>
+									<option value="15">15</option>
+									<option value="16">16</option>
+									<option value="17">17</option>
+									<option value="18">18</option>
+									<option value="19">19</option>
+									<option value="20">20</option>
+									<option value="21">21</option>
+									<option value="22">22</option>
+									<option value="23">23</option>
+									<option value="24">24</option>
+									<option value="25">25</option>
+									<option value="26">26</option>
+									<option value="27">27</option>
+									<option value="28">28</option>
+									<option value="29">29</option>
+									<option value="30">30</option>
+									<option value="31">31</option>
+									</select>
+									<select class="style2" name="month" style="display:inline-block;width:5.8em;">
+									<option value="0">Month</option>
+									<option value="1">Jan</option>
+									<option value="2">Feb</option>
+									<option value="3">Mar</option>
+									<option value="4">Apr</option>
+									<option value="5">May</option>
+									<option value="6">Jun</option>
+									<option value="7">Jul</option>
+									<option value="8">Aug</option>
+									<option value="9">Sept</option>
+									<option value="10">Oct</option>
+									<option value="11">Nov</option>
+									<option value="12">Dec</option>
+									</select>
+									<select class="style2" name="year" style="display:inline-block;width:5.2em;">
+									<option value="0">Year</option>
+									<option value="1">2018</option>
+									<option value="2">2017</option>
+									<option value="3">2016</option>
+									<option value="4">2015</option>
+									<option value="5">2014</option>
+									<option value="6">2013</option>
+									<option value="7">2012</option>
+									<option value="8">2011</option>
+									<option value="9">2010</option>
+									</select>
+									
+									<a style="display:inline-block; vertical-align:bottom;" href="">Why do i need to provide my<br>date of birth?</a>
+									</td></tr>
+									<tr><td><input type="radio" name="gender" value="female">Female&nbsp&nbsp&nbsp; 
+											<input type="radio" name="gender" value="male">Male	</td></tr>
+									<tr><td style="font-size:12px;color:#666666;">By clicking Sign Up, you agree to our <a href="">Terms</a>, <a href="">Data Policy</a>and<br> <a href="">Cookie Policy</a>. You may receive SMS notifications from us and<br> can opt out at any time.	</td></tr>
+									
+									<tr><td><button type="submit" name="signingup" id="signup" style="display:inline-block;">Sign Up</button></td></tr>
+									<tr><td style="margin=0px;padding:0px;opacity:.2;"> <hr> </td></tr>
+									<tr><td style="font-size:14px;font-weight:bold;color:#666666;"><a style="font-size:15px;"href="">Create a Page</a> for a celebrity, band or business.</td></tr>
+								</form>
+								</table>		
+							</td>
+						</tr>
+					</table>		
+				</td>
+			</tr>
+			<tr style="height:10.6em;"> 
+				<td>
+					<table class="inner1">
+						<tr>
+							<td>		</td>
+						</tr>
+					</table>		</td>
+			</tr>
+		</table>	
+	</body>
+</html>
+
